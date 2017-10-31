636c17c6366e1eecb15526614c06ef7bd718058c
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-10-31 14:52:58 -0400

    Creating multi-dimensional lists in Python

---
+++ Python 0022 - MultiDimList/MultiDimList.py
@@ -0,0 +1,19 @@
+# MultiDimList.py
+# created 10.02.2017
+
+# 2 Dimensional List
+x = [
+    [1,2],
+    [4,6],
+    [1,0]
+    ]
+
+print(x[1][0])
+
+# 3 Dimensional List
+y = [
+    [   [0,1],[2,3],[4,5]  ],
+    [   [6,7],[8,9]        ],
+    ]
+
+# [rows][columns][ints]
