5353c2d52fe23170579b3d0eebb3c6549fb2d1a9
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-10-31 14:34:52 -0400

    Append a .txt file

---
+++ Python 0014 - AppendFile/AppendFile.py
@@ -0,0 +1,8 @@
+# AppendFile.py
+# created 10.02.2017 by CB Fay
+
+appendMe = '\nNew information'
+
+appendFile = open('D:\Quick Access\Desktop\exampleFile.txt', 'a')
+appendFile.write(appendMe)
+appendFile.close
