daa7aebdf5a9bc28bf2cbc5bd3883d0a018e7331
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-10-30 23:20:14 -0400

    Adding Python to the PATH in windows Command Line

---
+++ Python 0001 - pathTutorial/pathTutorial.py
@@ -0,0 +1,8 @@
+# pathTutorial.py
+# define a string and print output to the user
+# created 09.28.2017 by CB Fay
+
+# how to use the command prompt to add the Python Interpreter to the path
+command = "set path = %path%;D:\Program Files\Python36"
+
+print("To add Python to your path, use %s"% (command))
