12bbeca2e6714fc684eb306ad47892f53b63c00a
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-10-31 14:38:11 -0400

    Using classes in Python

---
+++ Python 0016 - Classes/Classes.py
@@ -0,0 +1,23 @@
+# Classes.py
+# demonstrates how data and functionality is encapsulated inside a class
+# created 10.02.2017 by CB Fay
+
+class arithmetic:
+
+    def add(x,y):
+        sum = x + y
+        print(sum)
+
+    def sub(x,y):
+        dividend = x - y
+        print(dividend)
+
+    def mult(x,y):
+        product = x * y
+        print(product)
+
+    def div(x,y):
+        quotient = x / y
+        print(quotient)
+
+arithmetic.multiplication(3,5)
