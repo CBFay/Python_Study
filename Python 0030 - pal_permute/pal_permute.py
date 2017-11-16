5821d8969b5533bcc7ead88afec7d73ac4a8b86c
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-11-16 15:45:42 -0500

    Determine whether a string is a permutation of a palindrome (CCI)

---
+++ Python 0030 - pal_permute/pal_permute.py
@@ -0,0 +1,31 @@
+# Determine whether a string is a permutation of a palindrome
+# From pg 90 Gayle Laakman McDowell's "Cracking the Coding Interview"
+# created 11.16.2017 by CB Fay
+
+def pal_permute(str):
+	
+	letters = len(str)
+	list = [0] * 128
+	
+	for x in str:
+		if x == ' ': # consider spaces
+			letters -= 1
+		else:
+			list [char_num(x)] += 1 # consider uppercase
+
+	if letters % 2 == 0:
+		odd = 0
+	else:
+		odd = 1
+		
+	for x in str:
+		if list[char_num(x)] % 2 != 0: 
+			odd -= 1
+		if odd < 0:
+			return False
+			
+	return True
+	
+def char_num(character):
+	return ord(character.lower())
+	
