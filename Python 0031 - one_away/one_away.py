e992edec9a8950adae4138447bb95a8c998354e7
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-11-19 14:59:16 -0600

    Determine whether one string can be identical to another with a one character insert, removal, or replacement. (CCI)

--- Python 0031 - one_away/one_away.py
+++ Python 0031 - one_away/one_away.py
@@ -1,54 +1,38 @@
-# three operations: insert, delete, replace
-def one_away(A,B):
+# Determine whether one string can be identical to another with...
+# ...a one character insert, removal, or replacement.
+# From pg 91 of Gayle Laakman McDowell's "Cracking the Coding Interview"
+# created 11.19.2017 by CB Fay
+
+def one_away(a,b):
+    if a == b:
+        return True
     
-    lenDif = abs(len(A) - len(B))
+    lenDif = abs(len(a) - len(b))
     if lenDif > 1:
-        print("lenDif is too big")
         return False
-    
-    a = list(A)
-    b = list(B)
-    
-    if lenDif == 0:
-        return(can_replace(a,b))
-    
     if lenDif == 1:
         return(can_insert(a,b))
+    if lenDif == 0:
+        return(can_replace(a,b))
     
     
 def can_replace(a,b):
     charDif = 0
     
     for i in range(0, len(a), 1):
-        print(charDif)
         if a[i] != b[i]:
             charDif += 1
             if charDif > 1:
-                print("can't replace")
                 return False
-    print("can replace")
     return True
-    
-def can_insert(a,b):
-    if len(a) > len(b):
-        big = a
-        small = b
-    else:
-        big = b
-        small = a
-        
-    for i in range(0, len(small)-1, 1):
-        if small[i] != big[i] and small[i+1] != big[i]:
-            print("can't insert/delete")
-            return False
-    print("can insert/delete")
-    return True
-    
-one_away('hello', 'helo')
 
-# hlo
-# halo
-# hllo
-# hello
-# ello
-# helo
+
+def can_insert(a,b):
+    if len(a) < len(b):
+        a,b = b,a # swap
+        
+    for i in range(0, len(b)-1, 1):
+        if b[i] != a[i] and b[i+1] != a[i]:
+            return False
+    return True
+
