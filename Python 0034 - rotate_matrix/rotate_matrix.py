069e09402b608cabc9ece692e72d0e5992282f00
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-11-21 16:54:08 -0500

    Rotate a two dimensional list 90 degrees (CCI)

--- Python 0034 - rotate_matrix/rotate_matrix.py
+++ Python 0034 - rotate_matrix/rotate_matrix.py
@@ -1,11 +1,18 @@
+# Rotate a two dimensional list 90 degrees
+# From pg 91 of Gayle Laakman McDowell's "Cracking the Coding Interview"
+# created 11.21.2017 by CB Fay
+
 def rotate_matrix(matrix):
-    for y in range(0,2,1):
-        for x in range(0,2,1):
-            
+    halfway = len(matrix)/2
+
+    for y in range(0,halfway,1):
+        for x in range(0,halfway,1):
+          
+            # storing these Y,X pairs allows for in place rotation
             a = [x, y]
-            b = clockwise90([ a[0], a[1] ])
-            c = clockwise90([ b[0], b[1] ])
-            d = clockwise90([ c[0], c[1] ])
+            b = counterclock90([ a[0], a[1] ])
+            c = counterclock90([ b[0], b[1] ])
+            d = counterclock90([ c[0], c[1] ])
 
             # in place rotation
             matrix[a[0]][a[1]], \
@@ -18,23 +25,16 @@ def rotate_matrix(matrix):
             matrix[d[0]][d[1]], \
             matrix[a[0]][a[1]]
 
-    print(matrix[0])
-    print(matrix[1])
-    print(matrix[2])
-    print(matrix[3])
-
    
-def clockwise90(YX):
+def counterclock90(YX):
     return [YX[1], 3-YX[0]]
-    
-myMatrix = [[0, 1, 2, 3], 
-            [4, 5, 6, 7],
-            [8, 9, 10, 11],
-            [12, 13, 14, 15]]
 
+def clockwise90(YX):
+    return [3-YX[1], YX[0]]
+
+
+# test case
+myMatrix = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]
 rotate_matrix(myMatrix)
+print(myMatrix)
 
-# [0][0] -> [0][1] +0,1
-# [0][2] -> [2][3] +2,1
-# [1][2] -> [2][2] +1,0
-# [1][3] -> [3][2] +2,-1
