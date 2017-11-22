72c51cc78121b665f548f61f88e61bb28ef0d3a4
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-11-21 20:14:35 -0500

    If an element in an NxM matrix is 0, its entire row and column are set to 0 (CCI)

--- Python 0035 - zero_matrix/zero_matrix.py
+++ Python 0035 - zero_matrix/zero_matrix.py
@@ -1,7 +1,8 @@
-# If an element in an NxM matrix is 0, its entire row and column are set to 0.
+# If an element in an NxM matrix is 0, its entire row and column are set to 0
 # From pg 91 of Gayle Laakman McDowell's "Cracking the Coding Interview"
 # created 11.21.2017 by CB Fay
 
+# O(NM)
 def zero_matrix(matrix):
     zero_coordinates = []
     
@@ -13,7 +14,7 @@ def zero_matrix(matrix):
     for coords in zero_coordinates:
         zero_cross(matrix, coords)
     
-    
+# O(M+N)  
 def zero_cross(matrix, coordinates):
     for N in range(0, len(matrix), 1):
         matrix[N][coordinates[1]] = 0
@@ -21,7 +22,7 @@ def zero_cross(matrix, coordinates):
     for M in range(0, len(matrix[0]), 1):
         matrix[coordinates[0]][M] = 0
         
-        
+# O(N)       
 def print_matrix(matrix):
     for line in matrix:
         print(line)
@@ -30,13 +31,9 @@ def print_matrix(matrix):
 # test case
 myMatrix = [[1, 3, 4, 5, 8],    \
             [5, 3, 7, 1, 0],    \
-            [3, 1, 2, 0 , 9],   \
+            [3, 1, 2, 0, 9],    \
             [2, 3, 2, 5, 5],    \
             [6, 8, 7, 3, 2]]
 
-print_matrix(myMatrix)
-
 zero_matrix(myMatrix)
-print
-
 print_matrix(myMatrix)
