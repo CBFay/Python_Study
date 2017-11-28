823b90c9c1d488bfabb20612069fd80239a257aa
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-11-27 23:52:34 -0500

    Implementation of a Linked List

---
+++ Python 0039 - linked_list/linked_list.py
@@ -0,0 +1,29 @@
+# linked_list.py
+# Implementation of a Linked List
+# Created on 11.27.2017 by CB Fay
+
+class node:
+	def __init__(self):
+		self.data = None
+		self.next = None
+	def __str__(self):
+		return str(self.data)
+		
+class linked_list:
+	def __init__(self):
+		self.root = None # this will change every time we add a new node.
+		
+	
+	def add_node(self, data):
+		new_node = node() # create a new node
+		new_node.data = data
+		
+		new_node.next = self.root # link the new node to the 'previous' node.
+		self.root = new_node # set the current node to the new one.
+		
+	def list_print(self):
+		node = self.cur_node # can't point to ll!!
+		while node:
+			print (node.data)
+			node = node.next
+			
