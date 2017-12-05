ab3fd4ddef8d42390bc2d261dbd2e6e63b93e587
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-12-05 00:10:27 -0500

    Create a display window

--- Python 0045 - pyglet_demo/pyglet_demo.py
+++ Python 0045 - pyglet_demo/pyglet_demo.py
@@ -6,6 +6,8 @@ import pyglet
 class GameWindow(pyglet.window.Window):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
+		self.set_location(400, 200)
+		self.frame_rate= 1/60.0
 		
 	def on_draw(self):
 		self.clear()
@@ -15,5 +17,5 @@ class GameWindow(pyglet.window.Window):
 		
 if __name__ == "__main__":
 	window = GameWindow(640, 480, "My Window", resizable=False)
-	pyglet.clock.schedule_interval(window.update, 1/60.0)
+	pyglet.clock.schedule_interval(window.update, window.frame_rate)
 	pyglet.app.run()
