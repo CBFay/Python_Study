2d844a6ccd5a2f12c982b478aebd741d2e3fa854
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-12-10 22:34:53 -0500

    Experimentation with the pyglet module

--- Python 0046 - space_invaders/space_invaders.py
+++ Python 0046 - space_invaders/space_invaders.py
@@ -12,19 +12,46 @@ class GameWindow(pyglet.window.Window):
 		self.set_location(400, 200)
 		self.frame_rate= 1/60.0
 	
-		self.player = GameObject(320, 20, 'PLAYER.png')
-		# image = pyglet.image.load('res/sprites/PLAYER.png')
-		# self.player = pyglet.sprite.Sprite(image, x=320, y=20)
+		self.player = GameObject(80, 5, 'PLAYER.png')
+		
+		self.space_list = []
+		for i in range(2):
+			self.space_list.append(GameObject(0, i*160, 'SPACE.png'))
+		
+		for space in self.space_list:
+			space.vely = -100
+			
+	def update_space(self, dt):
+		for space in self.space_list:
+			space.update(dt)
+			if space.posy < -80:
+				self.space_list.remove(space)
+				self.space_list.append(GameObject(0, 80, 'SPACE.png')) 
+				for spc in self.space_list:
+					spc.vely = -200
+				
+	def on_key_press(self, symbol, modifiers):
+		if symbol == key.RIGHT:
+			self.player.velx = 200
+		if symbol == key.LEFT:
+			self.player.velx = -200
+			
+	def on_key_release(self, symbol, modifiers):
+		if symbol in (key.RIGHT, key.LEFT):
+			self.player.velx = 0
 		
 	def on_draw(self):
-		self.clear()
-		self.player.sprite.draw()
-		# self.player.draw()
+			self.clear()
+			for space in self.space_list:
+				space.draw()
+			self.player.draw()
 		
 	def update(self, dt):
-		pass
-		
+			self.player.update(dt) # 'data time'
+			self.update_space(dt) # 'data time'
+
+			
 if __name__ == "__main__":
-	window = GameWindow(640, 480, "Space Invaders", resizable=False)
+	window = GameWindow(192, 160, "Space Invaders", resizable=False)
 	pyglet.clock.schedule_interval(window.update, window.frame_rate)
 	pyglet.app.run()
