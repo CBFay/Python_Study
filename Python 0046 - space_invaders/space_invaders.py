4b97eece668defc838aee7269253caa2bf1fee14
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-12-11 00:17:41 -0500

    Update space_invaders.py

--- Python 0046 - space_invaders/space_invaders.py
+++ Python 0046 - space_invaders/space_invaders.py
@@ -1,22 +1,34 @@
 # Experimentation with the pyglet module
 # Created on 12.05.2017 by CB Fay
+# Updated on 12.10.2017
 
 import pyglet
-from pyglet.window import key
-from GameObjects import GameObject
+from pyglet.sprite import Sprite
+from pyglet.window import key, FPSDisplay
+from GameObjects import GameObject, preload_image
 
 # classes can take objects as parameters to inherit from them
 class GameWindow(pyglet.window.Window): 
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs) # necessary to initialize subclasses?
 		self.set_location(400, 200)
-		self.frame_rate= 1/60.0
-	
-		self.player = GameObject(80, 5, 'PLAYER.png')
+		self.frame_rate= 1/60.
+		self.fps_display = FPSDisplay(self)
+		self.fps_display.label.font_size = 6
+		self.fps_display.label.y = 5
+		self.fps_display.label.x = 5
+
+		self.right = False
+		self.left = False
+		self.player_speed = 100
+			
+		player_spr = Sprite(preload_image('PLAYER_10x.png'))
+		self.player = GameObject(80, 5, player_spr)
 		
 		self.space_list = []
+		self.space_img = preload_image('SPACE.png')
 		for i in range(2):
-			self.space_list.append(GameObject(0, i*160, 'SPACE.png'))
+			self.space_list.append(GameObject(0, i*160, Sprite(self.space_img)))
 		
 		for space in self.space_list:
 			space.vely = -100
@@ -24,30 +36,41 @@ class GameWindow(pyglet.window.Window):
 	def update_space(self, dt):
 		for space in self.space_list:
 			space.update(dt)
-			if space.posy < -80:
+			if space.posy <= -160:
 				self.space_list.remove(space)
-				self.space_list.append(GameObject(0, 80, 'SPACE.png')) 
-				for spc in self.space_list:
-					spc.vely = -200
+				self.space_list.append(GameObject(0, 160,  Sprite(self.space_img)))
+			space.vely = -100
 				
 	def on_key_press(self, symbol, modifiers):
 		if symbol == key.RIGHT:
-			self.player.velx = 200
+			self.right = True
 		if symbol == key.LEFT:
-			self.player.velx = -200
+			self.left = True
+		if symbol == key.ESCAPE:
+			pyglet.app.exit()
 			
 	def on_key_release(self, symbol, modifiers):
-		if symbol in (key.RIGHT, key.LEFT):
-			self.player.velx = 0
+		if symbol == key.RIGHT:
+			self.right = False
+		if symbol == key.LEFT:
+			self.left = False
 		
 	def on_draw(self):
 			self.clear()
 			for space in self.space_list:
 				space.draw()
 			self.player.draw()
+			self.fps_display.draw()
+
+	def update_player(self, dt):
+		self.player.update(dt)
+		if self.right and self.player.posx < 182:
+			self.player.posx += self.player_speed * dt
+		if self.left and self.player.posx > 1:
+			self.player.posx -= self.player_speed * dt
 		
 	def update(self, dt):
-			self.player.update(dt) # 'data time'
+			self.update_player(dt) # 'data time'
 			self.update_space(dt) # 'data time'
 
 			
