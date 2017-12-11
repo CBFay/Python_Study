8c602f2663efd9aba51f8a6b7eacfe921bf6b19c
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-12-10 22:34:23 -0500

    Update GameObjects.py

--- Python 0046 - space_invaders/GameObjects.py
+++ Python 0046 - space_invaders/GameObjects.py
@@ -9,3 +9,12 @@ class GameObject:
 		if image is not None:
 			image = pyglet.image.load('res/sprites/'+image)
 			self.sprite = pyglet.sprite.Sprite(image, x=self.posx, y=self.posy)
+			
+	def draw(self):
+		self.sprite.draw()
+		
+	def update(self, dt):
+		self.sprite.x += self.velx * dt
+		self.sprite.y += self.vely * dt
+		self.posx = self.sprite.x
+		self.posy = self.sprite.y
