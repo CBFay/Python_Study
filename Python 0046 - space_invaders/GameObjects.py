fed3cb0012cb49fe7998abddf2a4d4c686857a44
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-12-11 00:16:41 -0500

    Update GameObjects.py

--- Python 0046 - space_invaders/GameObjects.py
+++ Python 0046 - space_invaders/GameObjects.py
@@ -1,20 +1,27 @@
 import pyglet
 
+def preload_image(image):
+    img = pyglet.image.load('res/sprites/'+image)
+    return img
+
 class GameObject:
-	def __init__(self, posx, posy, image=None, hidden=False):
-		self.posx = posx # position
-		self.posy = posy
-		self.velx = 0 # velocity
-		self.vely = 0
-		if image is not None:
-			image = pyglet.image.load('res/sprites/'+image)
-			self.sprite = pyglet.sprite.Sprite(image, x=self.posx, y=self.posy)
-			
-	def draw(self):
-		self.sprite.draw()
+    def __init__(self, posx, posy, sprite=None, hidden=False):
+        self.posx = posx # position
+        self.posy = posy
+        self.velx = 0 # velocity
+        self.vely = 0
+        if sprite is not None:
+            self.sprite = sprite
+            self.sprite.x = self.posx
+            self.sprite.y = self.posy
+            self.width = self.sprite.width
+            self.height = self.sprite.height
+
+    def draw(self):
+        self.sprite.draw()
 		
-	def update(self, dt):
-		self.sprite.x += self.velx * dt
-		self.sprite.y += self.vely * dt
-		self.posx = self.sprite.x
-		self.posy = self.sprite.y
+    def update(self, dt):
+        self.posx += self.velx * dt
+        self.posy += self.vely * dt
+        self.sprite.x = self.posx
+        self.sprite.y = self.posy
