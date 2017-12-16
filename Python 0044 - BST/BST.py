bc7d3c2a32538fe0bd8249eda5ca4269fdaafec0
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-12-16 11:18:45 -0500

    Implementation of a Binary Search Tree

--- Python 0044 - BST/BST.py
+++ Python 0044 - BST/BST.py
@@ -1,89 +1,103 @@
-class Node:
-	def __init__(self, val):
-		self.value = val
-		self.left = None
-		self.right = None
-		
-	def insert(self, data):
-		if self.value == data:
-			return False
-		elif self.value > data: # LEFT
-			if self.left:
-				return self.left.insert(data)
-			else:
-				self.left = Node(data)
-				return True
-		elif self.value < data: # RIGHT
-			if self.right: 
-				return self.right.insert(data)
-			else:
-				self.right = Node(data)
-				return True
-				
-	def find(self, data):
-		if self.value == data:
-			return True
-		elif self.value < data: # LEFT
-			if self.left:
-				return self.left.find(data)
-			else:
-				return False
-		elif self.value > data: # RIGHT
-			if self.right:
-				return self.left.find(data)
-			else:
-				return False
-	
-	def preorder(self):
-		if self:
-			print(str(self.value), end = ' ') # print top down
-			if self.left:
-				self.left.preorder()
-			if self.right:
-				self.right.preorder()
+# Updated 12.16.2017 by CB Fay
 
-	def postorder(self):
-		if self:
-			if self.left:
-				self.left.postorder()
-			if self.right:
-				self.right.postorder() # print bottom up
-			print(str(self.value), end=' ') 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+		
+    def insert(self, data):
+        if self.data == data:
+            return False
+        elif self.data > data: # LEFT
+            if self.left:
+                return self.left.insert(data)
+            else:
+                self.left = Node(data)
+                return True
+        elif self.data < data: # RIGHT
+            if self.right: 
+                return self.right.insert(data)
+            else:
+                self.right = Node(data)
+                return True
+				
+    def find(self, data):
+        if self.data == data:
+            return True
+        elif self.data < data: # LEFT
+            if self.left:
+                return self.left.find(data)
+            else:
+                return False
+        elif self.data > data: # RIGHT
+            if self.right:
+                return self.left.find(data)
+            else:
+                return False
+	
+    def preorder(self):
+        if self:
+            print(str(self.data), end = ' ') # print top down
+            if self.left:
+                self.left.preorder()
+            if self.right:
+                self.right.preorder()
+
+    def postorder(self):
+        if self:
+            if self.left:
+                self.left.postorder()
+            if self.right:
+                self.right.postorder() # print bottom up
+            print(str(self.data), end=' ')
 			
-	def inorder(self):
-		if self:
-			if self.left:
-				self.left.inorder()
-			print(str(self.value), end = ' ') 
-			if self.right:
-				self.right.inorder() 
+    def inorder(self):
+        if self:
+            if self.left:
+                self.left.inorder()
+            print(str(self.data), end = ' ') 
+            if self.right:
+                self.right.inorder()
+    
+    def revorder(self):
+        if self:
+            if self.right:
+                self.right.revorder()
+            print(str(self.data), end = ' ')
+            if self.left:
+                self.left.revorder()
 
 
 		
 class Tree:
-	def __init__(self):
-		self.root = None
+    def __init__(self):
+        self.root = None
 		
-	def insert(self, data):
-		if self.root:
-			return self.root.insert(data)
-		else:
-			self.root = Node(data)
-			return True
+    def insert(self, data):
+        if self.root:
+            return self.root.insert(data)
+        else:
+            self.root = Node(data)
+            return True
    
-	def find(self, data):
-		if self.root:  
-			return self.root.find(data)
-		return False # else
+    def find(self, data):
+        if self.root:  
+            return self.root.find(data)
+        return False # else
 	
-	def preorder(self):
-		print("Preorder")
-		self.root.preorder()
+    def preorder(self):
+        print("Preorder")
+        self.root.preorder()
 
-	def postorder(self):
-		print("PostOrder")
-		self.root.postorder()
+    def postorder(self):
+        print("PostOrder")
+        self.root.postorder()
 		
-	def inorder(self):
-		print("InOrder")
-		self.root.inorder()
+    def inorder(self):
+        print("InOrder")
+        self.root.inorder()
+    
+    def revorder(self):
+        print("RevOrder")
+        self.root.revorder()
