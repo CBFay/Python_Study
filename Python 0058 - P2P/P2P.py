110d20e573f89574ea895dee4d20821645b65735
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-12-19 03:35:29 -0500

    Prototype of a P2P messaging application

--- Python 0058 - P2P/P2P.py
+++ Python 0058 - P2P/P2P.py
@@ -2,63 +2,58 @@
 # Created 12.18.2017
 
 import socket
-import threading
 import sys
+import time
 
-class Server:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connections = []
+class Host:
     def __init__(self):
-        myaddress = socket.gethostbyname(socket.gethostname())
-        print('My IPv4 address:', myaddress)
-        self.sock.bind(('0.0.0.0', 10000))
-        self.sock.listen(1)
-        
-    def handler(self, c, a):
-        while True:
-            data = c.recv(1024)
-            for connection in self.connections:
-                connection.send(data)
-            if not data:
-                self.connections.remove(c)
-                c.close
-                print(str('{} : {} disconnected'.format(a[0], a[1])))
-                break
-    
-    def run(self):
-        while True:
-            c, a = self.sock.accept()
-            cThread = threading.Thread(target=self.handler, args=(c,a))
-            cThread.daemon = True
-            cThread.start()
-            self.connections.append(c)
-            print(str('{} : {} connected'.format(a[0], a[1])))
+        s = socket.socket()
+        host = socket.gethostbyname(socket.gethostname())
+        print(host)
+        port = 10000
+        s.bind((host, port))
+        print('waiting for connection...')
+        s.listen(1)
+        conn, addr = s.accept()
+        print(addr, "has connected")  
 
-class Client:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-    def __init__(self, address):
-        self.sock.connect((address, 10000))
-        
-        iThread = threading.Thread(target=self.sendMsg)
-        iThread.daemon = True
-        iThread.start()
-        
         while True:
-            data = self.sock.recv(1024)
-            if not data:
-                break
-            print(str(data, 'utf-8'))    
-        
-    def sendMsg(self):
-        while True:
-            self.sock.send(bytes(input(""), 'utf-8'))
+            try:
+                message = input('>>> ').encode()
+                conn.send(message)
 
-            
-if(len(sys.argv)) > 1:
-    client = Client(sys.argv[1])
+                incoming = conn.recv(1024)
+                print(incoming.decode())
+            except KeyboardInterrupt:
+                sys.exit(0)
+                
+class Guest:
+    def __init__(self):
+        s = socket.socket()
+        host = input('Hostname: ')
+        port = 10000
+        s.connect((host,port))
+
+        while True:
+            try:
+                incoming = s.recv(1024)
+                print(incoming.decode())
+
+                message = input('>>> ').encode()
+                s.send(message)
+            except KeyboardInterrupt:
+                sys.exit(0)
+                
+relationship = None
+while True:
+    relationship = input('Will you be host or guest? ')
+    if relationship == 'host' or relationship == 'guest':
+        break
+    else:
+        print('Invalid response.')
+
+if relationship == 'host':
+    host = Host()
 else:
-    server = Server()
-    server.run()
-
-        
+    guest = Guest()
+    
