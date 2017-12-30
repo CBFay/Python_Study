acf6a6d3d51dbc9f0504def52c9bb3f71d5584f9
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-12-30 16:28:02 -0500

    Detect IPv4 and IPv6 addresses in a list

--- Python 0041 - checkIPs/checkIPs2.py
+++ Python 0041 - checkIPs/checkIPs2.py
@@ -1,16 +1,24 @@
 # A more succinct solution
-# Created 12.28.2017 by CB Fay
+# Created on 12.30.2017 by CB Fay
 
 import ipaddress
 
-def answer(address):
-    try:
-        addr = ipaddress.ip_address(address)
-        if type(addr) == ipaddress.IPv4Address:
-            return 'IPv4'
-        if type(addr) == ipaddress.IPv6Address:
-            return 'IPv6'
-    except ValueError:
-        return 'Neither'
-    
-print(answer('192.168.1.67'))
+def answer(addresses):
+    results = []
+    for x in addresses:
+        try:
+            addr = ipaddress.ip_address(x)
+            if type(addr) == ipaddress.IPv4Address:
+                results.append('IPv4')
+            elif type(addr) == ipaddress.IPv6Address:
+                results.append('IPv6')
+        except ValueError:
+            results.append('Neither')
+    return results
+
+# Test Case
+a = '132.1.222.80'
+b = '2001:0DBB:FA01:D33E:DB8:3333:93DA:3ED1'
+c = '267.23.554.9'
+
+print(answer([a, b, c]))
