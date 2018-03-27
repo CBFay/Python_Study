17cee5f56ad4fa1b36782dc2765922c788b3bc95
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2018-03-26 20:34:01 -0400

    define db tables from python

---
+++ Python 0071 - SQLAlchemy/models.py
@@ -0,0 +1,19 @@
+# define classes to represent tables in a db
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# represents a table in the db
+class Flight(db.Model):
+    __tablename__ = 'flights'
+    id = db.Column(db.Integer, primary_key=True)
+    origin = db.Column(db.String, nullable=False)
+    destination = db.Column(db.String, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+
+class Passenger(db.Model):
+    __tablename__ = 'passengers'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    flight_id = db.Column(db.Integer, db.ForeignKey("flights.id"), nullable=False) 
